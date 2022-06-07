4ed8f6f4d63948e9f8d6e621e6e6d889ed18f93d
Max4310 <93782481+Max4310@users.noreply.github.com>
2022-06-07 22:57:27 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+var rpc = require("discord-rpc")
+
+const client = new rpc.Client({ transport: 'ipc' })
+client.on('ready', () => {
+	client.request('SET_ACTIVITY', {
+		pid: process.pid,
+		activity : {
+			details : "Entra In Discord Italia",
+			assets : {
+				large_image : "discord_italia_",
+				large_text : "Entra In Discord Italia"
+			},
+			buttons : [{label : "DISCORD ITALIA" , url : "https://discord.gg/italia"}]
+		}
+	})
+})
+client.login({ clientId : "983477888886136864" }).catch(console.error);
+
